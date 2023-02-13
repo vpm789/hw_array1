3a3f1a960fa1c6983f312c27cc4da6df2b87a2ef
vpm789 <vpm789@gmail.com>
2023-02-13 13:27:34 +0300

    tasks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-
+        task1();
+        //task2();
+        //task3();
+        //task4();
     }
+    private static void task1() {
+        System.out.println("Задача №1");
+    }
+
+    private static void task2() {
+        System.out.println("");
+        System.out.println("Задача №2");
+    }
+
+    private static void task3() {
+        System.out.println("");
+        System.out.println("Задача №3");
+    }
+
+    private static void task4() {
+        System.out.println("");
+        System.out.println("Задача №4");
+    }
+
 }
